67c436dfabee80e8714f894da81b6add193a6a41
Gustavo Ospina <gospina@icbdholdings.com>
2024-11-17 12:24:14 -0500

    para la base funcionando

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthComponent } from './auth/auth.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SettingsComponent } from './settings/settings.component';
 import { UsersComponent } from './users/users.component';
+import { FuelComparisonComponent } from './fuel-comparison/fuel-comparison.component'; // Importar FuelComparisonComponent
 import { appRoutes } from './app.routes';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,15 @@ import { appRoutes } from './app.routes';
     AuthComponent,
     SidebarComponent,
     SettingsComponent,
-    UsersComponent
+    UsersComponent,
+    FuelComparisonComponent // Declarar FuelComparisonComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgxChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
